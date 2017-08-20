01c6ca1a8ee0920e8e6c2bf6d0af44361258a6d5
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-20 09:41:07 -0500

    Switching branches

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -1,4 +1,14 @@
 "use strict";
-app.controller("HomeCtrl", function($scope, $location){
+app.controller("HomeCtrl", function($scope, $location, UserFactory, GroupFactory){
+	//set user
+	let user = UserFactory.getUser();
+
+	$scope.activity = {};
+
+	GroupFactory.getGroupActivity()
+	.then( groupObj => {
+		$scope.groupActivity = groupObj;
+		console.log("$scope.groupActivity", $scope.groupActivity);
+	});
 
 });
