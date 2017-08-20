b4616521797921cfc86816d950fe79e21459b33f
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-08-20 16:42:43 -0500

    github working

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -26,7 +26,8 @@ app.controller("HomeCtrl", function($rootScope, $scope, $location, UserFactory, 
                 let username = response.username;
                 TaskFactory.getUsersTasks(user).then((response)=>{
                     TaskFactory.getTask(Object.keys(response)).then((response)=>{
-                        $scope.tasks.push({'username': username});
+                        console.log(response);
+                        $scope.tasks.push({'username': username, response});
                     });
                 });
                 GithubFactory.getGithubEvents(username).then((response)=>{
@@ -38,37 +39,11 @@ app.controller("HomeCtrl", function($rootScope, $scope, $location, UserFactory, 
                 });
             });
 
-            console.log($scope.tasks);
-        // if you want to be more clever...
+            console.log("$scope.tasks", $scope.tasks);
 
         });
     };
-    // let filterLists=(stored_tasks, github_tasks) => {
-    //     var result = stored_tasks.filter(function (task1) {
-    //         console.log(task1);
-    //         return github_tasks.some(function (task2) {
-    //             console.log(task2);
-    //             return task1.github_id === task2.github_id; // return the ones with equal id
-    //        });
-    //     });
-    //     console.log("result", result);
-    //     return result;
-    // };
- //    GithubFactory.getGithubUser('ZoeLeBlanc').then( (response)=>{
- //        console.log(response);
- //    });
- //    GroupFactory.getAllUsersGroup("group0").then((response)=>{
- //        console.log(response);
- //    });
- //    UserFactory.getAllUsers().then((response)=>{
- //        console.log(response);
- //        let all_users = response;
- //        all_users.forEach((name)=>{
- //            GithubFactory.getGithubEvents(name.username).then((response)=>{
- //                console.log(response);
- //            });
- //        });
- //    });
+ 
 	// GroupFactory.getGroupActivity()
 	// .then( groupObj => {
 	// 	$scope.groupActivity = groupObj;
