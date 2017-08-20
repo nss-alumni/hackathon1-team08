7c166169afde487dd69e1fc8d0448a17afc2f349
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-08-20 17:25:45 -0500

    added all our names

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -37,16 +37,18 @@ app.controller("HomeCtrl", function($rootScope, $scope, $location, UserFactory, 
 
                     });
                 });
+                let usernames = ['ZoeLeBlanc', 'Adobe-Android', 'elizawmeeks', 'jamestonkin','DominicSerranoC14'];
+                usernames.forEach((user)=>{
+                    GithubFactory.getGithubUser(user).then((response)=>{
+                    console.log("github", response);
+                    let githubEvents = response;
+                    githubEvents.forEach((event)=>{
+                         $scope.tasks.push({'github_id':event.actor.id, 'username': event.actor.login, 'github_date_create': event.created_at, 'payload': event.payload, 'public': event.public, 'repo_url':event.repo.url, 'type':event.type, 'uid':$rootScope.user.uid, 'completed':false});
 
-                // GithubFactory.getGithubUser($rootScope.user.username).then((response)=>{
-                //     console.log("github", response);
-
-                //     let githubEvents = response;
-                //     githubEvents.forEach((event)=>{
-                //          $scope.tasks.push({'github_id':event.actor.id, 'username': event.actor.login, 'github_date_create': event.created_at, 'payload': event.payload, 'public': event.public, 'repo_url':event.repo.url, 'type':event.type, 'uid':$rootScope.user.uid, 'completed':false});
-
-                //     });
-                // });
+                    });
+                });
+                });
+                
             });
 
             console.log("$scope.tasks", $scope.tasks);
