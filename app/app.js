94086b99c2cb10de62541a9af6ea9dd0e41ddc1c
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-08-20 08:11:28 -0500

    got navbar up and running, next hooking up auth page

--- app/app.js
+++ app/app.js
@@ -2,15 +2,17 @@
 
 const app = angular.module("CodeStars", ["ngRoute", 'ui.materialize']);
 
-app.config( $routeProvider => {
-	$routeProvider
-	.when("/", {
-        templateUrl: "partials/sample.html",
-        controller: "SampleCtrl"
-    })
-});
+let isAuth = (AuthFactory)=>{
+  new Promise( (resolve, reject)=>{
+    if (AuthFactory.isAuthenticated()){
+      resolve();
+    } else {
+      reject();
+    }
+  });
+};
 
-app.run((fbcreds)=>{
+app.run(($rootScope, $location, AuthFactory, fbcreds)=>{
    let cred = fbcreds;
    let authConfig = {
     apiKey: cred.apiKey,
@@ -19,4 +21,29 @@ app.run((fbcreds)=>{
    };
 
    firebase.initializeApp(authConfig);
+   $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
+    let logged = AuthFactory.isAuthenticated();
+    let appTo;
+    if (currRoute.originalPath){
+      appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+    }
+    console.log("appTo", appTo);
+    if(!appTo && !logged){
+      event.preventDefault();
+      $location.path('/auth');
+    }
+  });
+});
+
+app.config( $routeProvider => {
+  $routeProvider
+  .when("/", {
+        templateUrl: "partials/sample.html",
+        controller: "SampleCtrl"
+    })
+  .when('/', {
+      templateUrl: 'partials/auth.html',
+      controller: 'AuthCtrl'
+    })
+  .otherwise('/');
 });
