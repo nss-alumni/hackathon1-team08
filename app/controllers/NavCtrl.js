539f690abf8f2b4a56754b0120b033c170bbf006
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-08-20 09:41:02 -0500

    sidenav working

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,17 +1,4 @@
 "use strict";
 app.controller("NavCtrl", function($scope, $location){
-    $scope.navItems = [
-    {
-        name:"Logout", 
-        url:"#/logout"
-    },
-    {
-        name:"Home", 
-        url:"#/home"
-    },
-    {
-        name:"Progress", 
-        url:"#/progress"
-    }
-];
+
 });
